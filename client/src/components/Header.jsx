aff3d97fbec20846a021c2ed3fbef244fe8e434c
nvhiii <nahikhan16@gmail.com>
2024-05-02 02:23:19 -0400

    working on dashboard, onto plans + cart

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,33 +1,39 @@
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { Helmet } from "react-helmet";
 
 export default function Header() {
   // GETTER for the state
   const { currentUser } = useSelector((state) => state.user);
 
   return (
-    <div className="bg-orange-400">
-      <div className="flex justify-between items-center mx-auto max-w-6xl p-3">
-        <Link to="/">
-          <h1 className="font-bold">TA</h1>
-        </Link>
-        <ul className="flex gap-2 items-center">
+    <div>
+      <Helmet>
+        <title>TeachAssist</title>
+      </Helmet>
+      <div className="bg-orange-400">
+        <div className="flex justify-between items-center mx-auto max-w-6xl p-3">
           <Link to="/">
-            <li>Home</li>
+            <h1 className="font-bold">TA</h1>
           </Link>
-          <Link to="/about">{currentUser ? <li>Dashboard</li> : ""}</Link>
-          <Link to="/profile">
-            {currentUser ? (
-              <img
-                src={currentUser.pfp}
-                alt="profile-picture"
-                className="h-8 w-8 rounded-full object-cover"
-              />
-            ) : (
-              <li>Sign In</li>
-            )}
-          </Link>
-        </ul>
+          <ul className="flex gap-2 items-center">
+            <Link to="/">
+              <li>Home</li>
+            </Link>
+            <Link to="/about">{currentUser ? <li>Dashboard</li> : ""}</Link>
+            <Link to="/profile">
+              {currentUser ? (
+                <img
+                  src={currentUser.pfp}
+                  alt="profile-picture"
+                  className="h-8 w-8 rounded-full object-cover"
+                />
+              ) : (
+                <li>Sign In</li>
+              )}
+            </Link>
+          </ul>
+        </div>
       </div>
     </div>
   );
--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -5,13 +5,15 @@ import { useSelector } from "react-redux";
 export default function About() {
   const { currentUser } = useSelector((state) => state.user);
 
+  // add switch here to swap between pro / free plan
+
   return (
     <div className="px-4 py-12 max-w-2xl mx-auto">
       <div className="flex justify-between">
         {/* user greeting */}
         <div className="h-20 w-40 border-solid border-2 border-orange-400 rounded-lg p-3 flex flex-col">
           Welcome, {currentUser.username}
-          <div className="text-xs">Plan: </div>
+          <div className="text-xs">Plan: {}</div>
         </div>
         {/* plans, first check if plan purchased or not */}
         <div className="h-4 w-20 border-solid border-2 border-indigo-600">
